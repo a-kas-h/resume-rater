9cdbfbab1e17013b6b109606aedd0728f26cc0a7
Akash Saminathan <akashsaminathan24@gmail.com>
2025-07-13 23:17:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from PIL import Image
 import fitz  # PyMuPDF 
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY_1"))
 
 def get_gemini_response(input, pdf_content, prompt):
     model = genai.GenerativeModel('gemini-2.5-pro')
